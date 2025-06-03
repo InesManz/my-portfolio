73c65ab348ee1c9bc8abda057c6e853c6f0166d1
InesMP <inesmanz@gmail.com>
2025-06-03 18:15:00 +0200

    añado vite config para q se vean las imagenes

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,7 +2,7 @@ export const projects = [
     {
       title: "Proyecto de posicionamiento",
       description: "Este fue mi primer proyecto con css y html",
-      image:"./public/img/01posicionamiento.jpg",        
+      image:"/img/01posicionamiento.jpg",        
       tech: ["HTML5", "CSS3"],
       github: "https://github.com/InesManz/entrega_posicionamientos.git",
       link: "https://entrega-posicionamientos.vercel.app/",
@@ -11,7 +11,7 @@ export const projects = [
       title: "Flexbox",
       description: "Aprendiendo a usar Flexbox",
       image:
-        "./public/img/02flex-box.jpg",
+        "/img/02flex-box.jpg",
       tech: ["HTML5", "CSS3"],
       github: "https://github.com/InesManz/Entrega-Flexbox-InesManzano.git",
       link: "https://entrega-flexbox-ines-manzano.vercel.app/",
@@ -20,7 +20,7 @@ export const projects = [
       title: "Pagian con Grid",
       description: "Proyecto usando Grid",
       image:
-        "./public/img/03grd.jpg",
+        "/img/03grd.jpg",
         tech: ["HTML5", "CSS3"],
       github: "https://github.com/InesManz/Entrega-Grid-InesManzano.git",
       link: "https://entrega-grid-ines-manzano.vercel.app/",
@@ -29,7 +29,7 @@ export const projects = [
       title: "Mi primer proyecto",
       description: "En este proyecto uso tanto Grid como Flexbox.",
       image:
-        "./public/img/04primerproyecto.jpg",
+        "/img/04primerproyecto.jpg",
         tech: ["HTML5", "CSS3"],
       github: "https://github.com/InesManz/Entrega--Proyecto-1--InesManzano.git",
       link: "https://entrega-proyecto-1-ines-manzano.vercel.app/",
@@ -38,7 +38,7 @@ export const projects = [
       title: "Practica con DOM",
       description: "En esta practica empiezo a usar JavaScript",
       image:
-        "/public/img/05practicadom.jpg",
+        "/img/05practicadom.jpg",
       tech: ["HTML5", "CSS3", "JavaScript"],
       github: "https://github.com/InesManz/PracticaDom.git",
       link: "https://practica-dom-five.vercel.app/",
---
+++ vite.config.js
@@ -0,0 +1,6 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: './', // para q se vean las rutas de las imagenes bien
+})
